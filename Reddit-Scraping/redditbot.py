44a2eefbe3895d52502d4a745fdc61022d0164fe
Aidan Beery <aidanb1049@gmail.com>
2019-10-21 12:36:32 -0700

    Pull down all top-level comments in a submission

--- Reddit-Scraping/redditbot.py
+++ Reddit-Scraping/redditbot.py
@@ -1,7 +1,12 @@
 import praw
+from praw.models import MoreComments
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("music")
 
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=1):
     print("Title: ", submission.title)
+    for top_level_comment in submission.comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        print(top_level_comment.body)
