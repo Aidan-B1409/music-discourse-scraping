c61ae9211eee5291eb9da65de213fdb6776624f1
Aidan Beery <aidanb1049@gmail.com>
2019-11-04 14:09:17 -0800

    Introduction of relevency search feature

--- Reddit-Scraping/redditbot.py
+++ Reddit-Scraping/redditbot.py
@@ -1,13 +1,31 @@
 import praw
-from praw.models import MoreComments
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("music")
-n = 0
-for submission in subreddit.hot(limit=1):
-    print("Title: ", submission.title)
-    submission.comments.replace_more(limit=None)
-    for comment in submission.comments.list():
-        print(comment.body)
-        n = n + 1
-    print(n)
+
+def MineSubreddit(sub):
+    n = 0
+    for submission in sub.hot(limit=1):
+        print("Title: ", submission.title)
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            print(comment.body)
+            n = n + 1
+        print(n)
+
+
+def SearchSubreddit(sub, keyword):
+    #Creates a search query which is sorted by relevance to keyword, syntax = lucene, time-filter = all
+    for submission in sub.search(keyword, 'relevance', 'lucene', 'all'):
+        print(submission.title)
+
+def getSubreddits(data_txt):
+    #read subreddit data file
+    with open(data_txt) as f:
+        content = f.readlines()
+    #strip out newline characters
+    content = [x.strip('\n') for x in content]
+
+
+if __name__ == '__main__':
+    getSubreddits(r'subreddits.txt')
