820f300feae711662707c6dfb64bb32e436cfdc9
Aidan Beery <aidanb1049@gmail.com>
2020-02-06 14:24:51 -0800

    Reddit Bot v. 0.1
    Successfully pulls down comments for 1 query. Pre-refactor.

--- Reddit-Scraping/pulldownbot.py
+++ Reddit-Scraping/pulldownbot.py
@@ -4,28 +4,41 @@ from datetime import datetime
 
 reddit = praw.Reddit('bot1')
 """TODO - Make megasubreddit object based on subreddits.txt"""
-subreddit = reddit.subreddit("music")
+subreddit = reddit.subreddit("all")
 """TODO - Automate searching of multiple queries based on txt object"""
-query = 'Madeon'
+query = 'title:"Madeon" "All My Friends"'
 """Very important constant, defines how deep the getSubmissions method will search"""
 SEARCHLIMIT = 3
 
 def getSubmissions(keyword):
     searchResults = list()
-    for submission in subreddit.search(keyword, 'relevance', 'lucene', 'all',limit=SEARCHLIMIT):
+    for submission in subreddit.search(keyword, 'top', 'lucene', "all",limit=SEARCHLIMIT):
+        submission.comments.replace_more()
         searchResults.append(submission)
     return searchResults
 def mineComments(resultsList):
-    """counter for entries into CSV"""
+    """counter for CSV rows"""
     n = 0
     """Time of CSV initialization"""
-    dtime_string = datetime.now().strftime('%d/%m/%Y%H:%M:%S')
-    with open('reddit.csv', 'w') as csvfile:
-        fileWriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+    dtime_string = datetime.now().strftime('%d-%m-%Y%H-%M-%S')
+    with open('reddit_'+dtime_string+'.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        fileWriter.writerow(["Query Index", "Query", "Submission Index", "Subreddit", "Submission Title", "Submission ID", "Submission Score", "Comment", "Comment ID", "Comment Index", "Comment Score" ])
+
         for submission in resultsList:
-            for comment in submission.comments.list():
-                comm = comment.body.encode()
-                fileWriter.writerow([n,submission.id,submission.title,comm,comment.score,query,comment.subreddit.name,comment.id])
+            for comment in submission.comments:
+                submissionID = submission.id
+                submissionTitle = submission.title
+                commentBody = comment.body.replace(",","").encode('utf-8', "ignore");
+                commentBody = str(commentBody, encoding = 'utf-8');
+                commentScore = comment.score
+                entryQuery = query
+                subredditName = comment.subreddit.display_name
+                print(subredditName)
+                commentID = comment.id
+
+                fileWriter.writerow([n, submissionID, submissionTitle, commentBody, commentScore, entryQuery, subredditName, commentID])
+                n = n + 1
 
 
 
