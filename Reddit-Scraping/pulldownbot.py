0108ae9d161dcf8f1d9e71b701e325026f726f5d
Aidan Beery <aidanb1049@gmail.com>
2020-03-21 18:39:00 -0700

    Added progress bars

--- Reddit-Scraping/pulldownbot.py
+++ Reddit-Scraping/pulldownbot.py
@@ -1,22 +1,21 @@
 import praw
 import csv
 from datetime import datetime
-from pandas import DataFrame, read_csv
 import pandas as pd
 from query import Query
+from tqdm import tqdm
+import time
 
 reddit = praw.Reddit('bot1')
 """TODO - Make megasubreddit object based on subreddits.txt"""
 
 def readQueries():
-    queries = list()
     file = r'test.csv'
     df = pd.read_csv(file)[['artist_name', 'track_name']]
-    ##print(df.head(5))
     for index, row in df.iterrows():
         print(row['artist_name'], row['track_name'])
-        query = Query(row['artist_name'], row['track_name'], reddit);
-        mineComments(query.getSubmissions(), query);
+        query = Query(row['artist_name'], row['track_name'], reddit)
+        mineComments(query.getSubmissions(), query)
 
 def mineComments(resultsList, query):
     """counter for CSV rows"""
@@ -26,8 +25,7 @@ def mineComments(resultsList, query):
     with open('reddit_'+dtime_string+'.csv', 'w', encoding='utf-8', newline='') as csvfile:
         fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         fileWriter.writerow(["Comment Index", "Submission ID", "Submission Title", "Comment Body", "Comment Score", "Query", "Subreddit Name", "Comment ID"])
-
-        for submission in resultsList:
+        for submission in tqdm(resultsList):
             for comment in submission.comments:
                 submissionID = submission.id
                 submissionTitle = submission.title
@@ -36,11 +34,12 @@ def mineComments(resultsList, query):
                 commentScore = comment.score
                 entryQuery = query.query
                 subredditName = comment.subreddit.display_name
-                print(subredditName)
+                #print(subredditName)
                 commentID = comment.id
 
                 fileWriter.writerow([n, submissionID, submissionTitle, commentBody, commentScore, entryQuery, subredditName, commentID])
                 n = n + 1
+            time.sleep(0.1)
 
 
 
