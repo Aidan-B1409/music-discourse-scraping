ed165b68e8137db579be1d8676962bae985c3cec
Aidan Beery <aidanb1049@gmail.com>
2020-03-22 22:20:53 -0700

    Refactor, fixed CSV template

--- Reddit-Scraping/pulldownbot.py
+++ Reddit-Scraping/pulldownbot.py
@@ -1,49 +1,19 @@
 import praw
-import csv
-from datetime import datetime
+
 import pandas as pd
 from query import Query
-from tqdm import tqdm
-import time
 
 reddit = praw.Reddit('bot1')
-"""TODO - Make megasubreddit object based on subreddits.txt"""
-
 def readQueries():
     file = r'test.csv'
-    df = pd.read_csv(file)[['artist_name', 'track_name']]
+    df = pd.read_csv(file)
     for index, row in df.iterrows():
         print(row['artist_name'], row['track_name'])
         query = Query(row['artist_name'], row['track_name'], reddit)
-        mineComments(query.getSubmissions(), query)
-
-def mineComments(resultsList, query):
-    """counter for CSV rows"""
-    n = 0
-    """Time of CSV initialization"""
-    dtime_string = datetime.now().strftime('%d-%m-%Y%H-%M-%S')
-    with open('reddit_'+dtime_string+'.csv', 'w', encoding='utf-8', newline='') as csvfile:
-        fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        fileWriter.writerow(["Comment Index", "Submission ID", "Submission Title", "Comment Body", "Comment Score", "Query", "Subreddit Name", "Comment ID"])
-        for submission in tqdm(resultsList):
-            for comment in submission.comments:
-                submissionID = submission.id
-                submissionTitle = submission.title
-                commentBody = comment.body.replace(",","").encode('utf-8', "ignore");
-                commentBody = str(commentBody, encoding = 'utf-8');
-                commentScore = comment.score
-                entryQuery = query.query
-                subredditName = comment.subreddit.display_name
-                #print(subredditName)
-                commentID = comment.id
-
-                fileWriter.writerow([n, submissionID, submissionTitle, commentBody, commentScore, entryQuery, subredditName, commentID])
-                n = n + 1
-            time.sleep(0.1)
-
-
+        valence = row['valence']
+        arousal = row['arousal']
+        deezerID = row['dzr_sng_id']
+        query.mineComments(index, valence, arousal, deezerID)
 
 if __name__ == '__main__':
     readQueries()
-    #query = Query("Madeon", "All My Friends", reddit);
-    #mineComments(query.getSubmissions())
--- Reddit-Scraping/query.py
+++ Reddit-Scraping/query.py
@@ -1,4 +1,7 @@
-import praw
+import csv
+import datetime
+from tqdm import tqdm
+import time
 import csv
 from datetime import datetime
 
@@ -11,15 +14,51 @@ class Query:
         self.artist = artist
         self.reddit = reddit
         self.subreddit = reddit.subreddit("all")
-        self.query = Query.buildQuery(self, track, artist)
+        self.query = self.buildQuery(track, artist)
 
     def buildQuery(self, track, artist):
-        return str("title:" + "\"" + track + "\"" + " " +  "\"" + artist + "\"")
+        return str("title:" + "\"" + track + "\"" + " " + "\"" + artist + "\"")
 
     def getSubmissions(self):
         submissionList = list()
         subreddit = self.reddit.subreddit("all")
-        for submission in subreddit.search(self.query, 'top', 'lucene', "all", limit=Query.SEARCHLIMIT):
+        for submission in subreddit.search(self.query, 'top', 'lucene', "all", limit=self.SEARCHLIMIT):
             submission.comments.replace_more()
             submissionList.append(submission)
         return submissionList
+
+    def mineComments(self, queryIndex, valence, arousal, deezerID):
+        """Time of CSV initialization"""
+        dtime_string = datetime.now().strftime('%d-%m-%Y%H-%M-%S')
+        with open('reddit_'+dtime_string+'.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            fileWriter.writerow(["Query Index", "Query", "Valence", "Arousal", "Result Index", "Subreddit", "Subreddit ID",
+                                 "Submission Title", "Submission Body", "SubmissionID", "Comment Body", "Comment ID", "Comment Index", "Comment Replies",
+                                 "Comment Score", "Submission Comments", "Submission URL", "Submission Score", "Deezer ID"])
+            for resultIndex, submission in tqdm(enumerate(self.getSubmissions())):
+                for commentIndex, comment in enumerate(submission.comments):
+                    submissionID = submission.id
+                    submissionTitle = submission.title
+                    submissionBody = submission.selftext
+                    commentBody = comment.body.replace(",","").encode('utf-8', "ignore");
+                    commentBody = str(commentBody, encoding = 'utf-8');
+                    commentScore = comment.score
+                    commentID = comment.id
+                    subredditName = comment.subreddit.display_name
+                    subredditID = comment.subreddit.id
+                    commentID = comment.id
+                    commentReplies = len(comment.replies)
+                    submissionComments = submission.num_comments
+                    submissionURL = submission.url
+                    submissionScore = submission.score
+                    if commentBody.strip() is not "[deleted]":
+                        fileWriter.writerow(
+                            [queryIndex, self.query, valence, arousal, resultIndex, subredditName, subredditID,
+                             submissionTitle, submissionBody, submissionID, commentBody, commentID, commentIndex, commentReplies,
+                             commentScore, submissionComments, submissionURL, submissionScore, deezerID])
+                time.sleep(0.1)
+            else:
+                fileWriter.writerow(
+                    [queryIndex, self.query, valence, arousal, "", "", "", "",
+                     "", "", "End Of File", "", "", "", "",
+                     "", "", "", deezerID])
