1de243a14357a47e7d8aaa101807481fc62420a9
Aidan Beery <aidanb1049@gmail.com>
2020-03-29 16:24:25 -0700

    Refactor for styling

--- Reddit-Scraping/pulldownbot.py
+++ Reddit-Scraping/pulldownbot.py
@@ -1,39 +1,43 @@
 import praw
-import sys, getopt
+import sys
+import getopt
 import pandas as pd
 from query import Query
 
 reddit = praw.Reddit('bot1')
 
-def getArgs(argv):
-    inputFile = ""
+
+def get_args(argv: str):
+    input_file = ""
     try:
-        opts, args = getopt.getopt(argv, "hi:",["inputFile="])
+        opts, args = getopt.getopt(argv, "hi:", ["input_file="])
     except getopt.GetoptError:
         print("Invalid File Arguments Supplied! \n Valid Arguments Include: \n "
-              " 1: h (Help) \n 2: -i (Input CSV File) (Also accepts inputFile=)")
+              " 1: h (Help) \n 2: -i (Input CSV File) (Also accepts input_file=)")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-           print("Valid Arguments Include: \n 1: h (Help) \n  2: -i (Input CSV File) (Also accepts inputFile=)")
-           sys.exit(2)
-        elif opt in ("-i", "--inputFile"):
-            inputFile = arg
-    return inputFile
-def readQueries(inputFile):
-    df = pd.read_csv(inputFile.strip())
+            print("Valid Arguments Include: \n 1: h (Help) \n  2: -i (Input CSV File) (Also accepts input_file=)")
+            sys.exit(2)
+        elif opt in ("-i", "--input_file"):
+            input_file = arg
+    return input_file
+
+
+def read_queries(input_file: str) -> None:
+    df = pd.read_csv(input_file.strip())
     for index, row in df.iterrows():
         print(row['artist_name'], row['track_name'])
         query = Query(row['artist_name'], row['track_name'], reddit)
         valence = row['valence']
         arousal = row['arousal']
-        deezerID = row['dzr_sng_id']
-        query.mineComments(index, valence, arousal, deezerID)
+        deezer_id = row['dzr_sng_id']
+        query.mine_comments(index, valence, arousal, deezer_id)
 
 
 if __name__ == '__main__':
-    inputFile = getArgs(sys.argv[1:])
-    readQueries(inputFile)
-    #MANUAL TEST QUERY
-    #query = Query("Madeon", "All My Friends", reddit)
-    #query.mineComments("0", "0", "0", "0")
+    inputFile = get_args(sys.argv[1:])
+    read_queries(inputFile)
+    # MANUAL TEST QUERY
+    # search_keyword = Query("Madeon", "All My Friends", reddit)
+    # search_keyword.mineComments("0", "0", "0", "0")
--- Reddit-Scraping/query.py
+++ Reddit-Scraping/query.py
@@ -5,68 +5,71 @@ import time
 from datetime import datetime
 
 
-class Query:
-    query = "null"
-    SEARCHLIMIT = 3;
+def build_query(track: str, artist: str) -> str:
+    return str("title:" + "\"" + track + "\"" + " " + "\"" + artist + "\"")
 
-    def __init__(self, track, artist, reddit):
+
+class Query:
+    search_keyword = "null"
+    SEARCH_LIMIT = 3
+
+    def __init__(self, track: str, artist: str, reddit: object):
         self.track = track
         self.artist = artist
         self.reddit = reddit
         self.subreddit = reddit.subreddit("all")
-        self.query = self.buildQuery(track, artist)
+        self.search_keyword = build_query(track, artist)
 
-    def buildQuery(self, track, artist):
-        return str("title:" + "\"" + track + "\"" + " " + "\"" + artist + "\"")
-
-    def getSubmissions(self):
-        submissionList = list()
+    def get_submissions(self) -> list:
+        submission_list = list()
         subreddit = self.reddit.subreddit("all")
-        for submission in subreddit.search(self.query, 'top', 'lucene', "all", limit=self.SEARCHLIMIT):
+        for submission in subreddit.search(self.search_keyword, 'top', 'lucene', "all", limit=self.SEARCH_LIMIT):
             submission.comments.replace_more()
-            submissionList.append(submission)
-        return submissionList
+            submission_list.append(submission)
+        return submission_list
 
-    def mineComments(self, queryIndex, valence, arousal, deezerID):
+    def mine_comments(self, query_index: str, valence: str, arousal: str, deezer_id: str) -> None:
         """Time of CSV initialization"""
         dtime_string = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
-        fileName = 'reddit_' + dtime_string + "_" + str(deezerID) + ".csv"
-        with open(fileName, 'w', newline='', encoding='utf-8') as csvfile:
-            fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            fileWriter.writerow(["Query Index", "Query", "Valence", "Arousal", "Result Index", "Subreddit", "Subreddit ID",
-                                 "Submission Title", "Submission Body", "SubmissionID", "Comment Body", "Comment ID", "Comment Index", "Comment Replies",
-                                 "Comment Score", "Submission Comments", "Submission URL", "Submission Score", "Deezer ID"])
-            submissionsList = self.getSubmissions()
-            if len(submissionsList) > 0:
-                for resultIndex, submission in enumerate(tqdm(submissionsList)):
-                    for commentIndex, comment in enumerate(submission.comments):
-                        submissionID = submission.id
-                        submissionTitle = submission.title
-                        submissionBody = submission.selftext
-                        commentBody = comment.body
-                        commentScore = comment.score
-                        commentID = comment.id
-                        subredditName = comment.subreddit.display_name
-                        subredditID = comment.subreddit.id
-                        commentID = comment.id
-                        commentReplies = len(comment.replies)
-                        submissionComments = submission.num_comments
-                        submissionURL = submission.url
-                        submissionScore = submission.score
-                        if commentBody.strip() != "[deleted]":
-                            fileWriter.writerow(
-                                [queryIndex, self.query, valence, arousal, resultIndex, subredditName, subredditID,
-                                 submissionTitle, submissionBody, submissionID, commentBody, commentID, commentIndex, commentReplies,
-                                 commentScore, submissionComments, submissionURL, submissionScore, deezerID])
+        file_name = 'reddit_' + dtime_string + "_" + str(deezer_id) + ".csv"
+        with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+            file_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            file_writer.writerow(
+                ["Query Index", "Query", "Valence", "Arousal", "Result Index", "Subreddit", "Subreddit ID",
+                 "Submission Title", "Submission Body", "SubmissionID", "Comment Body", "Comment ID", "Comment Index",
+                 "Comment Replies", "Comment Score", "Submission Comments", "Submission URL", "Submission Score"
+                 "Deezer ID"])
+            submissions_list = self.get_submissions()
+            if len(submissions_list) > 0:
+                for result_index, submission in enumerate(tqdm(submissions_list)):
+                    for comment_index, comment in enumerate(submission.comments):
+                        submission_id = submission.id
+                        submission_title = submission.title
+                        submission_body = submission.selftext
+                        comment_body = comment.body
+                        comment_score = comment.score
+                        subreddit_name = comment.subreddit.display_name
+                        subreddit_id = comment.subreddit.id
+                        comment_id = comment.id
+                        comment_replies = len(comment.replies)
+                        submission_comments = submission.num_comments
+                        submission_url = submission.url
+                        submission_score = submission.score
+                        if comment_body.strip() != "[deleted]":
+                            file_writer.writerow(
+                                [query_index, self.search_keyword, valence, arousal, result_index, subreddit_name,
+                                 subreddit_id, submission_title, submission_body, submission_id, comment_body,
+                                 comment_id, comment_index, comment_replies, comment_score, submission_comments,
+                                 submission_url, submission_score, deezer_id])
                     time.sleep(0.1)
                 else:
-                    fileWriter.writerow(
-                        [queryIndex, self.query, valence, arousal, "", "", "", "",
+                    file_writer.writerow(
+                        [query_index, self.search_keyword, valence, arousal, "", "", "", "",
                          "", "", "End Of File", "", "", "", "",
-                         "", "", "", deezerID])
+                         "", "", "", deezer_id])
             else:
-                fileWriter.writerow(
-                    [queryIndex, self.query, valence, arousal, "", "", "", "",
+                file_writer.writerow(
+                    [query_index, self.search_keyword, valence, arousal, "", "", "", "",
                      "", "", "No Results", "", "", "", "",
-                     "", "", "", deezerID])
+                     "", "", "", deezer_id])
                 print("No Results")
