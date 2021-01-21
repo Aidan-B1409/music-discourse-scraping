a91638c8506bd88bea19327fa6606f0ec70c0b9b
Aidan-B1409 <aidanb1049@gmail.com>
2021-01-21 15:05:12 -0800

    Re-arranging

---
+++ util/feature_tool/csv_builder.py
@@ -0,0 +1,17 @@
+import csv
+
+class CSVBuilder:
+    def __init__(self, csvfile) -> None:
+        self.csvfile = csvfile
+        self.csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        self.writeheader()
+
+    def writeheader(self) -> None:
+        self.csvwriter.writerow(
+            ["Song ID"]
+            ["Song Name"]
+            ["Number of Comments"]
+            )
+
+    def writerow(self, features: list) -> None: 
+        self.csvwriter.writerow(features)
